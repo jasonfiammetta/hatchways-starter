d47e2094e22c64ea42d81a1a84d1bf7822aba229
jasonfiammetta <jason.fiammetta@gmail.com>
2022-01-07 18:54:11 -0500

    Corrected schema type

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema({
   user: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     require: true
   },
